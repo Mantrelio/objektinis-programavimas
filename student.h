2f411687b907997d018e7a824bcb5626d92cbdae
Mantrimas <mantrimas.kasparavicius@gmail.com>
2026-02-20 17:36:54 +0200

    feat: implement student struct

---
+++ student.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct Student {
+    string name, surname;
+    vector<int> homeworkGrades;
+    int examGrade;
+};
