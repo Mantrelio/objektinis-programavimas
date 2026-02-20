2d3bced6ef4a93b8ed700eb79157394789cd18f5
Mantrimas <mantrimas.kasparavicius@gmail.com>
2026-02-20 20:02:10 +0200

    feat: implement student-array header

---
+++ student-array.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct Student {
+    string name, surname;
+    int *homeworkGrades;
+    int examGrade;
+    float finalGrade;
+};
